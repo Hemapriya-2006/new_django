3aa528448d1dfa1b57256e3aea5c6178d4bbcb17
Hemapriya-2006 <hemapriyams2006@gmail.com>
2025-05-09 11:46:12 +0530

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,1 +1,6 @@
 pip install dj-database-url
+import dj-database-url
+import os
+DATABASES = {
+ "default": dj_database_url.parse(os.environ.get("postgresql://test_db_dw78_user:Qv3uJ0FvIb55tgTFQZrbIu1ypTkG6rx6@dpg-d0epgk95pdvs73au8udg-a.oregon-postgres.render.com/test_db_dw78"))
+}
